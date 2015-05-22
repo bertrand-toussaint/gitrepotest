b06366c530df42d2bf18df95a4955895d7c5d990
bertrand-toussaint <bertrand-toussaint@wanadoo.fr>
2015-05-22 15:01:45 +0200

    Mon premier commit sur TestBis

---
+++ gitestbis/src/gitestbis/GitTestBis.java
@@ -0,0 +1,10 @@
+package gitestbis;
+
+public class GitTestBis {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Git is hard");
+    }
+
+}
